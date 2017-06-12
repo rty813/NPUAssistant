7814b4990195c8c430e24f33b74c91ec1822bbd0
zhang <523213189@qq.com>
2017-06-12 12:53:36 +0800

    更换自定义Dialog

---
+++ app/src/main/java/com/npu/zhang/npuassistant/MyProgressDialog.java
@@ -0,0 +1,15 @@
+package com.npu.zhang.npuassistant;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by zhang on 2017/6/12.
+ */
+
+public class MyProgressDialog extends Dialog {
+    public MyProgressDialog(@NonNull Context context) {
+        super(context);
+    }
+}
